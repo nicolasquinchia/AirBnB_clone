a5f88b73d74e3fb506e4fa7ec916e64991c32ecd
monicajoa <mnortiz.ortiz@gmail.com>
2020-07-01 18:18:34 -0500

    AirBnB clone - The console, init constructor package module file

---
+++ models/__init__.py
@@ -0,0 +1,9 @@
+"""This module is the contructor for the
+    models Package and create an instance from
+    FilesStorage and reload all information in
+    the JSON file
+    """
+from models.engine.file_storage import FileStorage
+
+storage = FileStorage()
+storage.reload()
