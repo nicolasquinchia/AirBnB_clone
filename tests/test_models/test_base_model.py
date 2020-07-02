4acb1547f0f09de48d52e5150ec590dca9075101
monicajoa <mnortiz.ortiz@gmail.com>
2020-07-01 22:15:59 -0500

    AirBnB clone - The console, add test base model file

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,22 +1,21 @@
 #!/usr/bin/python3
 """Test base_model file
-    """
+"""
 
-    import unittest
-    import os
-    import pep8
-    from datetime import datetime
-    from models.base_model import BaseModel
+import unittest
+import pep8
+import os
+from datetime import datetime
+from models.base_model import BaseModel
+
 
 class TestBase(unittest.TestCase):
-    """Test Base
-
-    Args:
-        unittest ([test]):
+    """Tests for the BaseModel class
     """
-    def test_pep8_style(self):
+
+    def test_basemodel_pep8(self):
         """Test PEP8 style
         """
         pep8style = pep8.StyleGuide(quiet=True)
-        result = pep8style.check_files(["models/base_model.py"])
+        result = pep8style.check_files(["./models/base_model.py"])
         self.assertEqual(result.total_errors, 0)
